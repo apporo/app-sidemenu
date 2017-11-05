3b5f7ffceca3647d9b8abd419d4d1e77461d36e1
dat <hbd.dn92@gmail.com>
2017-11-06 02:05:16 +0700

    add jquery plugin flypanels

--- public/assets/sidemenu/js/loader.js
+++ public/assets/sidemenu/js/loader.js
@@ -2,4 +2,10 @@ $(document).ready(function(){
   $('#menu').multilevelpushmenu({
     menu: menuData
   });
+
+  // $('.flypanels-container').flyPanels({
+  // 	treeMenu: {
+  //       init: true
+  //     }
+  // });
 });
